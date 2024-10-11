1ddc6acaf943e4debf5d9c90273b93f87d042757
tanyaschmitt2 <tanyamarius@gmail.com>
2024-10-11 13:43:42 -0400

    Imported the pieces from the pieces autos json file,  created DOM element and attached the element to the rest of the document using appendChild function in Javascript

---
+++ pieces.js
@@ -0,0 +1,21 @@
+// Récupération des pièces depuis le fichier JSON
+const reponse = await fetch("pieces-autos.json");
+const pieces = await reponse.json();
+
+//Created new DOM elements
+const article = pieces[0];
+const imageElement = document.createElement("img");
+imageElement.src = article.image;
+const nomElement = document.createElement("h2");
+nomElement.innerText = article.nom;
+const prixElement = document.createElement("p");
+prixElement.innerText = `Prix: ${article.prix} €`;
+const categorieElement = document.createElement("p");
+categorieElement.innerText = article.categorie;
+
+//Attached the DOM elements to the rest of the document
+const sectionFiches = document.querySelector(".fiches");
+sectionFiches.appendChild(imageElement);
+sectionFiches.appendChild(nomElement);
+sectionFiches.appendChild(prixElement);
+sectionFiches.appendChild(categorieElement);

9a3b4873bd01cd2367ed6b87414d89e9738db422
tanyaschmitt2 <tanyamarius@gmail.com>
2024-10-11 15:03:47 -0400

    test

--- pieces.js
+++ pieces.js
@@ -4,24 +4,35 @@ const pieces = await reponse.json();
 
 //Created new DOM elements
 const article = pieces[0];
+
 const imageElement = document.createElement("img");
 imageElement.src = article.image;
+
 const nomElement = document.createElement("h2");
 nomElement.innerText = article.nom;
+
 const prixElement = document.createElement("p");
 prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+
 const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie;
+categorieElement.innerText = article.categorie ?? "(aucune categorie)";
+
+//Creation of DOM elements for the exercise
+const descriptionElement = document.createElement("p");
+descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+
+const stockElement = document.createElement("p");
+stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+
 
 //Attached the DOM elements to the rest of the document
 const sectionFiches = document.querySelector(".fiches");
+
 sectionFiches.appendChild(imageElement);
 sectionFiches.appendChild(nomElement);
 sectionFiches.appendChild(prixElement);
 sectionFiches.appendChild(categorieElement);
 
-
-document.body.appendChild(prixElement);
-
-//Nullish operator
-categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+// Ajout des element au DOM pour l'exercice
+sectionFiches.appendChild(descriptionElement);
+sectionFiches.appendChild(stockElement);
